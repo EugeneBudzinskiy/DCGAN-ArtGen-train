084047d3fda7928a64b9bde5c12f5d4ce960e317
EugeneBudzinskiy <eugene.budzinskiy@gmail.com>
2023-05-23 10:29:13 +0300

    Add docstring to `config.py`

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import model_zoo
 
-
+# List of all possible model and their corresponding main file
 MODELS = {
     "DCGAN": model_zoo.DCGAN,
     "DCGAN_WI": model_zoo.DCGAN_WI,
     "WGAN_GP": model_zoo.WGAN_GP
 }
 
+# Current model name
 MODEL_NAME = "WGAN_GP"
 
+# Path for saving pretrained model
 SAVE_GENERATOR_PATH = f"xtr_generators/{MODEL_NAME}"
